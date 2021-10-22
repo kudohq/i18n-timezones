2d12600762c177ebd8e469a40a5da9e2f0642942
Randy Antler <randy@kudoway.com>
2021-10-22 09:31:58 -0700

    Simplify test somewhat

--- spec/unit/timezone_spec.rb
+++ spec/unit/timezone_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe ActiveSupport::TimeZone do
+  before { I18n.locale = locale }
+
   describe '#to_s' do
     let(:locale) { :de }
 
-    before do
-      I18n.locale = locale
-      load File.join(File.dirname(__FILE__), '../../lib/i18n_timezones/timezone.rb')
-    end
+    before { load File.join(File.dirname(__FILE__), '../../lib/i18n_timezones/timezone.rb') }
 
     it 'localizes the timezone name that appears within the string representation' do
       zone = ActiveSupport::TimeZone.new('American Samoa')
@@ -17,8 +16,6 @@ describe ActiveSupport::TimeZone do
 
   describe '#name' do
     let(:locale) { :fr }
-
-    before { I18n.locale = locale }
 
     it 'localizes the timezone name' do
       expect(ActiveSupport::TimeZone.new('Chatham Is.').name).to eq('Îles Chatham')
